2f8ab58333cb387a181cb681ea1241d6efbba332
leidy1014 <145583179+leidy1014@users.noreply.github.com>
2023-09-20 10:21:33 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<h1>PROYECTO PHP</h1>
+
+<?php
+ //tipos de variables;
+/*    $nombre = "Daniel";
+    $edad = 25;
+    $float = 1.80;
+    $bolean= true;
+    $var1 = 7;
+    $var2 = 15;
+
+    echo "Este es un texto";
+    echo "<h2>APRENDIENDO PHP</h2>";
+    echo "<h2>$nombre</h2>";
+    echo $nombre." ".$edad;
+    echo "<br>";
+    echo "<br>";
+    echo $var1+$var2."\n";
+
+// if
+    if ($bolean==true){
+        echo "ESTO ES UN BOOLEAN";
+    }
+    else {
+        echo "ESTE NO ES UN BOOLEAN";
+    }
+//imprimir con consola 
+    echo "<script>console.log('$nombre')</script>";
+*/
+
+//ejercicio de intercambiar variables 
+    $varX = 15;
+    $varY = 25;
+
+    
+   // $varX = $varX+$varY;//40
+    //$varY = $varX-$varY;//15/
+    //$varX = $varX-$varY;
+
+    //echo "esta es la variable x :". $varX;
+    //echo "esta es la variable y :". $varY;
+    
+    //$array = array(1,2,3,4,5);
+    //print_r($array);
+    //var_dump($array);
+
+    echo"<br><br>";
+
+    //$array2 = array (1,2,"texto",4,true);
+    //var_dump($array2);
+
+    echo"<br><br>";
+
+    //$array3 = array(
+    //    "1" => 45,
+      //  "x" => "y",
+        //"test" => true
+    //);
+
+    
+    //var_dump($array3);
+
+    //echo"<br><br>";
+
+    //$array4 = array("ford","mazda","ferrari","volvo"); 
+    //var_dump($array4);
+
+
+
+    //$array[] = 8;
+    //unset ($array[1]);
+    //var_dump($array);
+
+    //echo count ($array);//contar
+    //echo"<br><br>";
+    //echo sizeof($array); //contar 
+
+    //sort($array);// menor a mayor 
+    //print_r($array);
+
+    echo"<br><br>";
+
+    //rsort($array4);//mayor a menor
+    //print_r($array4);
+
+
+    echo"<br><br>";
+
+    in_array("mazda",$array4); // devuelve ub verdadero o falso si esta dentro de array
+    array_search("ferrari",$array4); //
+
+
+
+
+?>
